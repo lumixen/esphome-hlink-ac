78a8c91f0ef1fca62dc489b56af834b39e4da025
Max Bodaniuk <max.bodaniuk@gmail.com>
2025-03-22 16:01:57 +0200

    Impl

--- components/hlink_ac/hlink_ac.cpp
+++ components/hlink_ac/hlink_ac.cpp
@@ -8,6 +8,10 @@ namespace esphome
     namespace hlink_ac
     {
         static const char *const TAG = "hlink_ac";
+        static const uint8_t CMD_TERMINATION_SYMBOL = 0x0D;
+    
+        FeatureType features[] = { POWER_STATE, MODE, TARGET_TEMP };
+        constexpr int features_size = sizeof(features) / sizeof(features[0]);
 
         void HlinkAc::setup()
         {
@@ -19,43 +23,52 @@ namespace esphome
         void HlinkAc::dump_config()
         {
             ESP_LOGCONFIG(TAG, "Hlink AC component:");
-            ESP_LOGCONFIG(TAG, "  Requested update: %s", this->requested_update_ ? "true" : "false");
-            ESP_LOGCONFIG(TAG, "  Requested sequence: %d", this->requested_sequence_number_);
+            ESP_LOGCONFIG(TAG, "  Requested sequence: %d", this->requested_feature_);
         }
 
         void HlinkAc::request_status_update_()
         {
-            this->requested_update_ = true;
+            if (this->requested_feature_ != -1) {
+                this->requested_feature_ = 0;
+                this->requested_update_ms = millis();
+            }
         }
 
         void HlinkAc::loop()
         {
-            if (this->requested_update_ && this->requested_sequence_number_ == -1) {
-                uint16_t p_value = 1;
-                uint16_t c_value = p_value ^ 0xFFFF;
-                char buf[20] = {0};
-                int size = sprintf(buf, "MT P=%04X C=%04X\x0D", p_value, c_value);
-                this->write_str(buf);
-                this->requested_sequence_number_ = 1;
+            if (this->requested_feature_ != -1 && !this->receiving_response_) {
+                this->send_status_update_request_(features[this->requested_feature_]);
             }
             
-            if (this->requested_sequence_number_ != -1 && this->available() > 2) {
-                const int MAX_BUFFER_SIZE = 30;  // Define max buffer size
-                uint8_t response_buffer[MAX_BUFFER_SIZE] = {0};  // Fixed-size buffer
-                int index = 0;
-            
-                while (this->available() && index < MAX_BUFFER_SIZE - 1) {  
-                    this->read_byte(&response_buffer[index]);
-                    if (response_buffer[index] == 0x0D) {  // Stop reading when stop byte (0x0D) is encountered
-                        break;
-                    }
-                    index++;
-                }
-                this->requested_sequence_number_ = -1;
-                this->requested_update_ = false;
+            if (this->requested_feature_ != -1) {
+                this->read_status_update_();
             }
             
         }
+        
+        void HlinkAc::send_status_update_request_(FeatureType feature_type) {
+            uint16_t p_value = feature_type;
+            uint16_t c_value = p_value ^ 0xFFFF; // Calculate checksum
+            char buf[18] = {0};
+            int size = sprintf(buf, "MT P=%04X C=%04X\x0D", p_value, c_value);
+            this->write_str(buf);
+            // ESP_LOGD(TAG, "Requested address %04X", p_value);
+        }
 
+        void HlinkAc::read_status_update_() {
+            if (this->available() > 2) {
+                uint8_t response_buffer[30] = {0};
+                int index = 0;
+                while (response_buffer[index] != CMD_TERMINATION_SYMBOL && millis() - requested_update_ms < 4000) {
+                    this->read_byte(&response_buffer[++index]);
+                }
+                // Request next feature from features sequence or reset to none if done
+                if (this->requested_feature_ + 1 >= features_size) {
+                    this->requested_feature_ = -1;
+                } else {
+                    this->requested_feature_++;
+                }
+            }
+        }
     }
 }
--- components/hlink_ac/hlink_ac.h
+++ components/hlink_ac/hlink_ac.h
@@ -7,6 +7,12 @@ namespace esphome
 {
   namespace hlink_ac
   {
+    enum FeatureType : uint16_t {
+      POWER_STATE = 0x0000,
+      MODE = 0x0001,
+      TARGET_TEMP = 0x0003
+    };
+
     class HlinkAc : public Component, public uart::UARTDevice
     {
     public:
@@ -15,10 +21,12 @@ namespace esphome
       void dump_config() override;
 
     protected:
-      // unsigned long last_sent_time_ = 0;
-      bool requested_update_ = false;
-      int8_t requested_sequence_number_ = -1;
+      bool receiving_response_ = false;
+      int8_t requested_feature_ = -1;
+      uint32_t requested_update_ms = 0;
       void request_status_update_();
+      void send_status_update_request_(FeatureType feature_type);
+      void read_status_update_();
     };
   }
 }
