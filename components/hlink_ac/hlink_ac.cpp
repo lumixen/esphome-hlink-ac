d93a5f2b32ca893bf5c906a6ba614d461c6ff89a
Max Bodaniuk <max.bodaniuk@gmail.com>
2025-03-23 00:21:16 +0200

    Impl

--- components/hlink_ac/hlink_ac.cpp
+++ components/hlink_ac/hlink_ac.cpp
@@ -8,11 +8,20 @@ namespace esphome
     namespace hlink_ac
     {
         static const char *const TAG = "hlink_ac";
+
         static const uint8_t CMD_TERMINATION_SYMBOL = 0x0D;
         static const uint32_t STATUS_UPDATE_INTERVAL = 6500;
         static const uint32_t STATUS_UPDATE_TIMEOUT = 2000;
+
+        const HlinkResponse HLINK_RESPONSE_NOTHING = {HlinkResponse::Status::PROCESSING};
+        const HlinkResponse HLINK_RESPONSE_INVALID = {HlinkResponse::Status::INVALID};
+          
+        static const std::string OK_TOKEN = "OK";
+        static const std::string NG_TOKEN = "NG";
+
+        // static const std::map<uint32_t, 
     
-        // Status update AC features
+        // AC status features
         FeatureType features[] = { POWER_STATE, MODE, TARGET_TEMP, SWING_MODE, FAN_MODE };
         constexpr int features_size = sizeof(features) / sizeof(features[0]);
 
@@ -30,7 +39,7 @@ namespace esphome
 
         void HlinkAc::request_status_update_()
         {
-            if (this->status_.state == PENDING) {
+            if (this->status_.state == IDLE) {
                 // Launch update sequence
                 this->status_.state = REQUEST_NEXT_FEATURE;
                 this->status_.requested_feature = 0;
@@ -46,20 +55,20 @@ namespace esphome
             }
             
             if (this->status_.state == READ_NEXT_FEATURE) {
-                bool success = this->read_cmd_response_(50);
-                if (success) {
+                HlinkResponse response = this->read_cmd_response_(50);
+                if (response.status == HlinkResponse::Status::OK) {
                     if (this->status_.requested_feature + 1 < features_size) {
                         this->status_.state = REQUEST_NEXT_FEATURE;
                         this->status_.requested_feature++;
                     } else {
-                        this->status_.state = PENDING;
+                        this->status_.state = IDLE;
                     }
                 }
             }
             
             // Reset update status if we reached timeout
-            if (this->status_.state != PENDING && millis() - this->status_.status_changed_at_ms > STATUS_UPDATE_TIMEOUT) {
-                this->status_.state = PENDING;
+            if (this->status_.state != IDLE && millis() - this->status_.status_changed_at_ms > STATUS_UPDATE_TIMEOUT) {
+                this->status_.state = IDLE;
                 ESP_LOGW(TAG, "Reached timeout while updating H-link AC status.");
             }
         }
@@ -76,12 +85,12 @@ namespace esphome
             this->write_str(buf);
         }
 
-        bool HlinkAc::read_cmd_response_(uint32_t timeout_ms) {
+        HlinkResponse HlinkAc::read_cmd_response_(uint32_t timeout_ms) {
             if (this->available() > 2) {
                 uint32_t started_millis = millis();
                 std::string response(30, '\0');
                 int read_index = 0;
-                // Read response unless carriage return symbol or timeout
+                // Read response unless carriage return symbol, timeout or buffer overflow
                 while (millis() - started_millis < timeout_ms || read_index < 30) {
                     this->read_byte((uint8_t*)&response[read_index]);
                     if (response[read_index] == CMD_TERMINATION_SYMBOL) {
@@ -97,17 +106,29 @@ namespace esphome
                         last_space_i = i + 1;
                     }
                 }
-                ESP_LOGD(TAG, "Response tokens size: %d", response_tokens.size());
-                for (int i = 0; i < response_tokens.size(); i++) {
-                    ESP_LOGD(TAG, "Token %d: %s", i, response_tokens[i].c_str());
-                }
+                // ESP_LOGD(TAG, "Response tokens size: %d", response_tokens.size());
+                // for (int i = 0; i < response_tokens.size(); i++) {
+                //     ESP_LOGD(TAG, "Token %d: %s", i, response_tokens[i].c_str());
+                // }
                 if (response_tokens.size() != 3) {
                     ESP_LOGW(TAG, "Invalid H-link response: %s", response.c_str());
-                    return false;
+                    return HLINK_RESPONSE_INVALID;
                 }
-                return true;
+
+                HlinkResponse::Status status;
+                if (response_tokens[0] == OK_TOKEN) {
+                    status = HlinkResponse::Status::OK;
+                } else if (response_tokens[0] == NG_TOKEN) {
+                    status = HlinkResponse::Status::NG;
+                } else {
+                    return HLINK_RESPONSE_INVALID;
+                }
+                uint32_t p_value = std::stoi(response_tokens[1], nullptr, 16);
+                uint16_t checksum = std::stoi(response_tokens[2], nullptr, 16);
+                ESP_LOGD(TAG, "Received H-link response. Status: %s, P: %04X, C: %04X", status == HlinkResponse::Status::OK ? "OK" : "NG", p_value, checksum);
+                return { status, p_value, checksum };
             }
-            return false;
+            return HLINK_RESPONSE_NOTHING;
         }
     }
 }
--- components/hlink_ac/hlink_ac.h
+++ components/hlink_ac/hlink_ac.h
@@ -1,5 +1,6 @@
 #pragma once
 
+// #include "esphome/components/climate/climate.h"
 #include "esphome/core/component.h"
 #include "esphome/components/uart/uart.h"
 
@@ -7,7 +8,16 @@ namespace esphome
 {
   namespace hlink_ac
   {
-    enum FeatureType : uint16_t {
+
+    struct DeviceStatus
+    {
+      bool *power_state = nullptr;
+      uint16_t *target_temperature = nullptr;
+      // climate::ClimateSwingMode swing_mode,
+    };
+
+    enum FeatureType : uint16_t
+    {
       POWER_STATE = 0x0000,
       MODE = 0x0001,
       TARGET_TEMP = 0x0003,
@@ -15,18 +25,35 @@ namespace esphome
       FAN_MODE = 0x0002
     };
 
-    enum State : uint8_t {
-      PENDING,
+    enum HlinkComponentState : uint8_t
+    {
+      IDLE,
       REQUEST_NEXT_FEATURE,
       READ_NEXT_FEATURE,
+      APPLY_CONTROLS,
+      PUBLISH_CLIMATE_UPDATE
     };
 
-    struct HlinkAcStatus {
-      State state = PENDING;
+    struct HlinkResponse
+    {
+      enum class Status
+      {
+        PROCESSING,
+        OK,
+        NG,
+        INVALID
+      };
+      Status status;
+      uint32_t p_value;
+      uint16_t checksum;
+    };
+
+    struct ComponentStatus
+    {
+      HlinkComponentState state = IDLE;
       uint32_t status_changed_at_ms = 0;
       int8_t requested_feature = 0;
     };
-    
 
     class HlinkAc : public Component, public uart::UARTDevice
     {
@@ -36,10 +63,11 @@ namespace esphome
       void dump_config() override;
 
     protected:
-      HlinkAcStatus status_ = HlinkAcStatus();
+      ComponentStatus status_ = ComponentStatus();
+      DeviceStatus device_status_ = DeviceStatus();
       void request_status_update_();
       void write_cmd_request_(FeatureType feature_type);
-      bool read_cmd_response_(uint32_t timeout_ms);
+      HlinkResponse read_cmd_response_(uint32_t timeout_ms);
     };
   }
 }
