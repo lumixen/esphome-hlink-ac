4d833eba7e77b3614c3f1e39774cf95e667d15c3
Max Bodaniuk <max.bodaniuk@gmail.com>
2025-03-24 15:53:48 +0200

    Impl

--- components/hlink_ac/hlink_ac.cpp
+++ components/hlink_ac/hlink_ac.cpp
@@ -81,7 +81,7 @@ namespace esphome
                     break;
                 case HlinkResponseFrame::Status::INVALID:
                 case HlinkResponseFrame::Status::NG:
-                    ESP_LOGW(TAG, "Error readon next status update, falling back to IDLE state.");
+                    ESP_LOGW(TAG, "Error reading next status update, falling back to IDLE state.");
                     this->status_.state = IDLE;
                 }
             }
