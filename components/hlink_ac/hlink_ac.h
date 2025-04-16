8bfc2216a23ae879289be6ec3d95df610da84f38
Max Bodaniuk <max.bodaniuk@gmail.com>
2025-04-16 22:36:02 +0300

    Add remote lock switch

--- components/hlink_ac/hlink_ac.h
+++ components/hlink_ac/hlink_ac.h
@@ -4,6 +4,10 @@
 #include "esphome/components/uart/uart.h"
 #include "esphome/components/climate/climate.h"
 
+#ifdef USE_SWITCH
+#include "esphome/components/switch/switch.h"
+#endif
+
 namespace esphome
 {
   namespace hlink_ac
@@ -32,7 +36,7 @@ namespace esphome
       ACK_APPLIED_REQUEST
     };
 
-    struct HvacStatus
+    struct HlinkEntityStatus
     {
       optional<bool> power_state;
       optional<float> current_temperature;
@@ -40,6 +44,7 @@ namespace esphome
       optional<esphome::climate::ClimateMode> mode;
       optional<esphome::climate::ClimateFanMode> fan_mode;
       optional<esphome::climate::ClimateSwingMode> swing_mode;
+      optional<bool> remote_control_lock;
       bool ready()
       {
         return power_state.has_value() && current_temperature.has_value() && target_temperature.has_value() && mode.has_value() && fan_mode.has_value() && swing_mode.has_value();
@@ -71,6 +76,9 @@ namespace esphome
     constexpr uint16_t HLINK_FAN_MEDIUM = 0x0002;
     constexpr uint16_t HLINK_FAN_LOW = 0x0003;
     constexpr uint16_t HLINK_FAN_QUIET = 0x0004;
+
+    constexpr uint16_t HLINK_REMOTE_LOCK_ON = 0x0001;
+    constexpr uint16_t HLINK_REMOTE_LOCK_OFF = 0x0000;
 
     struct HlinkRequestFrame
     {
@@ -159,6 +167,13 @@ namespace esphome
 
     class HlinkAc : public Component, public uart::UARTDevice, public climate::Climate
     {
+      #ifdef USE_SWITCH
+      public:
+        void set_remote_lock_switch(switch_::Switch *sw);
+        void enqueue_remote_lock_action(bool state);
+      protected:
+        switch_::Switch *remote_lock_switch_{nullptr};
+      #endif
     public:
       // Component overrides
       void setup() override;
@@ -170,12 +185,12 @@ namespace esphome
 
     protected:
       ComponentStatus status_ = ComponentStatus();
-      HvacStatus hvac_status_ = HvacStatus();
+      HlinkEntityStatus hlink_entity_status_ = HlinkEntityStatus();
       CircularRequestsQueue pending_action_requests;
       void request_status_update_();
       void write_feature_status_request_(FeatureType feature_type);
-      void apply_feature_response_to_hvac_status_(FeatureType requested_feature, HlinkResponseFrame feature_response);
-      void publish_climate_update_if_any_();
+      void apply_feature_response_to_hlink_entity_(FeatureType requested_feature, HlinkResponseFrame feature_response);
+      void publish_updates_if_any_();
       HlinkResponseFrame read_hlink_frame_(uint32_t timeout_ms);
       void write_hlink_frame_(HlinkRequestFrame frame);
       std::unique_ptr<HlinkRequestFrame> createRequestFrame_(uint16_t primary_control, uint16_t secondary_control, optional<HlinkRequestFrame::AttributeFormat> secondary_control_format = HlinkRequestFrame::AttributeFormat::TWO_DIGITS);
