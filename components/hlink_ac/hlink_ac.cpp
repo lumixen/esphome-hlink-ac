dbe9b4f03efe68b8b0576ed6578f7fbd257bdb22
Max Bodaniuk <max.bodaniuk@gmail.com>
2025-05-02 22:03:12 +0300

    Comments

--- components/hlink_ac/hlink_ac.cpp
+++ components/hlink_ac/hlink_ac.cpp
@@ -54,10 +54,12 @@ void HlinkAc::setup() {
                target_temperature >= 0xFF00) {
              this->hlink_entity_status_.target_temperature = NAN;
              // In auto mode the target temperature control is not available
-             // Instead AC expects temperature shifts in range [-3;+3] C
+             // Instead, AC expects temperature offset in range [-3;+3] C
              // AUTO HEATING: FFFD -> FFFF, FFFE -> FF00, FFFF -> FF01, FF00 -> FF02, FF01 -> FF03, FF02 -> FF04, FF03
-             // -> FF05 AUTO COOLING: FFFD -> FFFB, FFFE -> FFFC, FFFF -> FFFD, FF00 -> FFFE, FF01 -> FFFF, FF02 ->
+             // -> FF05
+             // AUTO COOLING: FFFD -> FFFB, FFFE -> FFFC, FFFF -> FFFD, FF00 -> FFFE, FF01 -> FFFF, FF02 ->
              // FF00, FF03 -> FF01
+             // Needs testing, it's not clear if offset makes any difference in real life
              int8_t offset_temp = static_cast<int8_t>(target_temperature - 0xFF00);
              if (this->hlink_entity_status_.hlink_climate_mode == HLINK_MODE_HEAT_AUTO) {
                this->hlink_entity_status_.target_temperature_auto_offset = offset_temp - 2;
